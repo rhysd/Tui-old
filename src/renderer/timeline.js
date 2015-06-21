113311bbe00581475b12b41eb305cbd9d6bf6e89
rhysd <lin90162@yahoo.co.jp>
2015-06-22 03:21:42 +0900

    add timline.js

---
+++ src/renderer/timeline.js
@@ -0,0 +1,21 @@
+var tweets = [
+        {
+            icon: "../resources/inu.jpeg",
+            name: "@Linda_pp",
+            text: "ワンワン"
+        },
+        {
+            icon: "../resources/inu.jpeg",
+            name: "@Linda_pp",
+            text: "(U'w') I think this is a text"
+        },
+        {
+            icon: "../resources/inu.jpeg",
+            name: "@Linda_pp",
+            text: "WOW! foooooooooooooooooooooooooooooooooooooooooooooooooooooooooO!!!"
+        }
+    ];
+
+function hometimeline() {
+    return tweets;
+}
