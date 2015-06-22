c91058ba775f420874367d2177c42a5dd9a193bf
rhysd <lin90162@yahoo.co.jp>
2015-06-22 19:41:08 +0900

    add base of YAML config file

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,20 +1,34 @@
 var app = require('app');
 var path = require('path');
 var BrowserWindow = require('browser-window');
+var config = require('./config.js').load(app.getPath('userData'));
+var ipc = require('ipc');
+
 
 require('crash-reporter').start();
 
+// Main Window {{{
 var mainWindow = null;
 
 app.on('window-all-closed', function(){ app.quit(); });
 
 app.on('ready', function(){
-        mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600});
 
-        html = 'file://' + path.resolve(__dirname, '..', '..', 'static', 'index.html');
-        mainWindow.loadUrl(html);
+    html = 'file://' + path.resolve(__dirname, '..', '..', 'static', 'index.html');
+    mainWindow.loadUrl(html);
 
-        mainWindow.on('closed', function(){
-            mainWindow = null;
-        });
+    mainWindow.on('closed', function(){
+        mainWindow = null;
+    });
 });
+// }}}
+
+// IPC {{{
+ipc.on('require-config', function(event, arg){ // Sync
+    console.log('main: reply-config');
+    event.returnValue = config;
+});
+// }}}
+
+
