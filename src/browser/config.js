4323dec513c5d619d53b0b66e8d68ce0019b5986
rhysd <lin90162@yahoo.co.jp>
2015-06-23 09:57:08 +0900

    add config

---
+++ src/browser/config.js
@@ -0,0 +1,18 @@
+var yaml = require('js-yaml');
+var path = require('path');
+var fs = require('fs');
+
+// Note:
+// Add Config class to allow to deal with default config and user
+// customization
+
+module.exports.load = function(dir){
+    var file = path.join(dir, 'config.yml');
+    try {
+        return yaml.load(fs.readFileSync(file));
+    } catch(e) {
+        console.log('No configuration file is found: ' + e);
+        console.log('If you want to customize this app, please create ' + file);
+        return {};
+    }
+};
