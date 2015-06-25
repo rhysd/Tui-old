21d9fac1764b1ecf44874cbae12e54473c2ec9f5
rhysd <lin90162@yahoo.co.jp>
2015-06-25 11:02:23 +0900

    format datetime string

--- src/renderer/twitter.js
+++ src/renderer/twitter.js
@@ -2,6 +2,13 @@ var ipc = require('ipc');
 
 var callbacks = [];
 ipc.on('twitter-stream', function(tweet){
+    var created_at = tweet.created_at;
+    if (created_at !== undefined) {
+        var d = new Date(created_at);
+        tweet.created_at = `${d.toLocaleTimeString()} ${d.getMonth()}/${d.getDate()} ${d.getYear() + 1900}`;
+        console.log('date: ' + tweet.created_at);
+    }
+
     for (var c of callbacks) {
         c(tweet);
     }
