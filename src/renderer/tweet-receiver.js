a41be6b3ae81c86febabee9b92120f10a58494c1
rhysd <lin90162@yahoo.co.jp>
2015-06-30 19:38:49 +0900

    add retweet statuses

--- src/renderer/tweet-receiver.js
+++ src/renderer/tweet-receiver.js
@@ -5,12 +5,6 @@ var ipc = require('ipc');
 var callbacks = [];
 
 ipc.on('twitter-stream', function(tweet){
-    const created_at = tweet.created_at;
-    if (created_at !== undefined) {
-        const d = new Date(created_at);
-        tweet.created_at = `${d.getHours()}:${d.getMinutes()} ${d.getMonth()+1}/${d.getDate()} ${d.getYear() + 1900}`;
-    }
-
     for (let c of callbacks) {
         c(tweet);
     }
